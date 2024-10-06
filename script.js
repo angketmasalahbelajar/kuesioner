ea40f61c3cad22381b8b27f4d3680e41a625d69c
giadev25 <argiamahya1602@gmail.com>
2024-10-06 07:14:58 +0700

    fix: adding script.js file

---
+++ script.js
@@ -0,0 +1,28 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwVh04h3Y6-pPRGO1wLsvMVt6e8x7EorehOduzOsOT2t9F8Rzpc5G7LaD6BH0LE3CNk/exec'
+const form = document.forms['submit-to-google-sheet']
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // ketika tombol submit di klik
+    // tampilkan tombol loading hilangkan tombol kirim
+    btnLoading.classList.toggle('d-none')
+    btnKirim.classList.toggle('d-none')
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        btnLoading.classList.toggle('d-none')
+        btnKirim.classList.toggle('d-none')
+        Swal.fire({
+            title: 'Berhasil 😁',
+            text: 'Silahkan dilanjutkan',
+            icon: 'success',
+            confirmButtonText: 'lanjutkan'
+        })
+
+        form.reset()
+
+        console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+})
